1f80dc006f25fb2e5beaba82153a8d6ecfe9e350
Audrie8a <ann.audrie8a@gmail.com>
2022-03-26 17:54:23 -0600

    borrando claves

--- Backend/Keys/creds.js
+++ Backend/Keys/creds.js
@@ -1,8 +1,8 @@
 let aws_keys = {
     s3: {
         region: 'us-east-2',
-        accessKeyId: "AKIAS5BMI5IOEUTT6JEK",
-        secretAccessKey: "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",
+        accessKeyId: "---------------------------------",
+        secretAccessKey: "---------------------------------",
         //apiVersion: '2006-03-01',
     },
     dynamodb: {
